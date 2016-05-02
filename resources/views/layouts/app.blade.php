623814b4054e85c4f159a5bf49a2f2a7f4d39ca0
HuarenYU <yuhuarenproject@gmail.com>
2016-05-02 22:21:46 +0800

    add class

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
     <title>来我家呗 - @yield('title')</title>
-    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+    <link rel="stylesheet" href="http://121.41.8.56{{ elixir('css/app.css') }}">
     @yield('stylesheet')
 </head>
 <body @yield('bodyClass')>
